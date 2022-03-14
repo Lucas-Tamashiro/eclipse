c3661b3772daa2357ffca7400ec292cac9592b51
Lucas Tamashiro <67601655+Lucas-Tamashiro@users.noreply.github.com>
2022-03-14 13:56:49 -0300

    Adcionando arquivos via upload
    Teste de upload eclipse java-GitHub

---
+++ Hello GitHub/src/LucasTamashiroGitHub/HelloGitHub.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package LucasTamashiroGitHub;
+
+/**
+ * @author Shiro
+ *Teste de conexão projeto java com GitHub
+ */
+public class HelloGitHub {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHub");
+	}
+
+}
